66b1998881aa499685528f4b3d23acd51068762a
FelipeSepulvedaRozas <felipe.s.rozas@gmail.com>
2018-09-07 02:03:27 -0300

    create controller completeds for view completed tasks

--- ToDo2.0/config/routes.rb
+++ ToDo2.0/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'todo/index'
   devise_for :users
 
-  resources :todo,only: :index
+  resources :todo,only: :index do
+  	resources :completeds,only: :create
+  end
     root to: 'todo#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
