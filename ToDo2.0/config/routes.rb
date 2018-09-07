8610282a6f33b349c738956a878682be906a1fff
FelipeSepulvedaRozas <felipe.s.rozas@gmail.com>
2018-09-07 00:24:11 -0300

    creation todo model and Populating the Database with seeds.rb

--- ToDo2.0/config/routes.rb
+++ ToDo2.0/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'todo/index'
   devise_for :users
+
+  resources :todo,only: :index
+    root to: 'todo#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- ToDo2.0/db/seeds.rb
+++ ToDo2.0/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Todo.create(name: 'Bailar Cueca',description: 'a mover el bote', photo: 'cueca.png')
+Todo.create(name: 'Tomar terremoto',description: 'beber hasta ver triple', photo: 'drink.png')
+Todo.create(name: 'Asado',description: 'Comer carne hasta reventar', photo: 'food.png')
+Todo.create(name: 'tomar mote con huesillo',description: 'pa la caña', photo: 'recovery.png')
+Todo.create(name: 'elevar un volantin',description: 'kill them all', photo: 'vs.png')
+Todo.create(name: 'poner la bandera',description: 'igual no lo haras', photo: 'flag.png')
+Todo.create(name: 'comer empanadas',description: 'no mas dieta', photo: 'food2.png')
+Todo.create(name: 'resureccion de los muertos',description: 'cuantos curados encuentras en la calle', photo: 'zombie.png')
+Todo.create(name: 'Tomar paracetamol',description: 'recuperarse de la caña', photo: 'healty.png')
