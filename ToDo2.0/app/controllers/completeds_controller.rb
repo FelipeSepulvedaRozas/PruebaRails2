561e638f3945c4b634dd59174d2566f178e1f417
FelipeSepulvedaRozas <felipe.s.rozas@gmail.com>
2018-09-09 02:39:00 -0300

    updating code again

--- ToDo2.0/app/controllers/completeds_controller.rb
+++ ToDo2.0/app/controllers/completeds_controller.rb
@@ -3,39 +3,33 @@ class CompletedsController < ApplicationController
 
 
   def create
- 
-  	@todo = Todo.find(params[:todo_id])
+    @todo = Todo.find(params[:todo_id])
   	@user = current_user
   	@task = Completed.where(:user_id => @user, :todo_id => @todo)
   	if @task.empty?
-		@completed = Completed.new(todo: @todo, user: current_user, status: true)
-	  	if @completed.save
-	  		redirect_to root_path, notice:'Estado completado'
-	  	else
-	  		redirect_to root_path, alert:"No se pudo completar" 
-	  	end
+		  @completed = Completed.new(todo: @todo, user: current_user, status: true)
+	  	  if @completed.save
+	  		 redirect_to root_path, notice:'Estado completado'
+	  	  else
+	  		 redirect_to root_path, alert:"No se pudo completar" 
+	  	  end
   	else
   		if Completed.where(:user_id => @user, :todo_id => @todo).update_all(:status => true)
   			redirect_to root_path, notice:'Estado Completado'
   		else
   			redirect_to root_path, alert:"No se pudo completar"	
-  		end
-	  	
-	end	
+  		end 	
+	 end	
   end
 
   def update
   	@todo = Todo.find(params[:todo_id])
   	@user = current_user
-  	if Completed.where(:user_id => @user, :todo_id => @todo).update_all(:status => false)
-  		redirect_to root_path,notice:'Estado Corregido'
-  	else
+  	  if Completed.where(:user_id => @user, :todo_id => @todo).update_all(:status => false)
+  		  redirect_to root_path,notice:'Estado Corregido'
+  	  else
   		redirect_to root_path, alert:"No se pudo corregir"	
   	end
-  end
-
-  def completed_params
-  	params.require(:completed).permit(:user_id, :todo_id, :status => false)
   end
 
   def index
--- ToDo2.0/app/controllers/todo_controller.rb
+++ ToDo2.0/app/controllers/todo_controller.rb
@@ -1,33 +1,30 @@
 class TodoController < ApplicationController
 def index
-     @status = []
-  	@Todos = Todo.all
+  @status = []
+  @Todos = Todo.all
     if user_signed_in?
-      @Status = Completed.select(:user_id, :todo_id).where(:user_id => current_user, :status => true).all
+        @Status = Completed.select(:user_id, :todo_id).where(:user_id => current_user, :status => true).all
       @Status.each do |status|
         @status << status.todo_id 
       end
       @completed = @status.length
       @all = Todo.all.count
     end
-
   end
 
   def show
-  @status = []
+    @status = []
     @users = []
     @Todo= Todo.find(params[:id])
-    if user_signed_in?  
+      if user_signed_in?  
         @users = User.joins(:completeds).where(completeds: {todo_id: @Todo, status: true})
         @Status = Completed.select(:user_id, :todo_id).where(:user_id => current_user, :status => true).all
         @Status.each do |status|
-        @status << status.todo_id 
-    end
-      @completed = @status.length
-      @all = Todo.all.count
-      @Top = User.joins(:completeds).where(completeds: {todo_id: @Todo, status: true}).limit(5)
-    end
+          @status << status.todo_id 
+        end
+        @completed = @status.length
+        @all = Todo.all.count
+        @Top = User.joins(:completeds).where(completeds: {todo_id: @Todo, status: true}).limit(5)
+      end
   end
-
-
 end
